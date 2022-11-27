0b9e90c3f980d8990703021310008206c7d9ed8f
Misterio <Misterriobit@gmail.com>
2022-11-27 21:05:39 +0700

    выполнил доп. задание

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-'use strict';
+// 'use strict';
 
-let money;
-let income;
-let addExpenses; 
-let deposit;
-let mission;
-let period;
+let money = 90000;
+let income = 'фриланс';
+let addExpenses = 'Хобби, Коммуналка, Интернет, Авто'; 
+let deposit = false;
+let mission = 150000;
+let period = 6;
+let budgetDay = money / 30;
+
+
+console.log('я умею писать на javascript');
+ 
+console.log(typeof money, income, deposit);
+console.log(addExpenses.length);
+console.log("Период равен " + period + " месяцев \nЦель заработать "+ mission +" рублей");
+console.log(addExpenses.toLocaleLowerCase().split(', '));
+console.log( budgetDay);
+
+let num  = 266219;
+let str = String(num);
+let i;
+
+let sum = 1;
+
+for  (let digit  of str) {
+  sum *= Number (digit);
+
+}
+console.log(sum);
